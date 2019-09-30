eb57d81c25251157b2a04297b5fb212cd6053eb6
dohliam <dohliam@gmail.com>
2019-09-30 01:53:24 -0700

    option to specify multiple tables

--- webtable_to_text.rb
+++ webtable_to_text.rb
@@ -19,12 +19,52 @@ def print_table(options, table_string)
   end
 end
 
+def single_table(tables, options, numstring)
+  num = numstring.to_i
+
+  if num > tables.length
+    abort("  Table number out of range")
+  end
+
+  if options[:interactive]
+    puts "  Converting table #{numstring}..."
+  else
+    puts "==Table ##{numstring}=="
+  end
+
+  table_string = tables[num - 1].inner_html
+
+  print_table(options, table_string)
+end
+
+def multiple_tables(tables, options, numstring)
+  num_array = numstring.split(",")
+  num_array.each do |n|
+    if !n.match(/\d/) then next end
+    single_table(tables, options, n)
+    puts
+  end
+  exit
+end
+
+def all_tables(tables, options)
+  counter = 1
+  tables.each do |table|
+    table_string = table.inner_html
+    puts "==Table ##{counter.to_s}=="
+    print_table(options, table_string)
+    puts
+    counter += 1
+  end
+  exit
+end
+
 options = {}
 OptionParser.new do |opts|
   opts.banner = "  Usage: webtable_to_text.rb [options]"
 
   opts.on("-a", "--all", "Print all tables found on the specified page") { options[:all] = true }
-  opts.on("-n", "--number NUM", "Print specific table number only") { |v| options[:number] = v }
+  opts.on("-n", "--number NUM", "Print specific table number only; separate multiple numbers with commas") { |v| options[:number] = v }
   opts.on("-c", "--csv", "Output in CSV / comma separated values format") { options[:csv] = true }
   opts.on("-f", "--file FILE", "Specify HTML file as source for extracting tables") { |v| options[:file] = v }
   opts.on("-i", "--interactive", "Interactive mode") { options[:interactive] = true }
@@ -61,30 +101,12 @@ if options[:interactive]
   numstring = $stdin.gets.chomp
 end
 
+if numstring.match(/,/)
+  multiple_tables(tables, options, numstring)
+end
+
 if !numstring.match(/^\d+$/)
-  counter = 1
-  doc.xpath('//table').each do |table|
-    table_string = table.inner_html
-    puts "==Table ##{counter.to_s}=="
-    print_table(options, table_string)
-    puts
-    counter += 1
-  end
-  exit
+  all_tables(tables, options)
 end
 
-num = numstring.to_i
-
-if num > len
-  abort("  Table number out of range")
-end
-
-if options[:interactive]
-  puts "  Converting table #{numstring}..."
-else
-  puts "==Table ##{numstring}=="
-end
-
-table_string = doc.xpath('//table')[num - 1].inner_html
-
-print_table(options, table_string)
+single_table(tables, options, numstring)
