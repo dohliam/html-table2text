cb1a0139d7443fd1b677f9bce1ccc05da7fd26d5
dohliam <dohliam@gmail.com>
2019-10-02 02:23:49 -0700

    add option to read from input file

--- webtable_to_text.rb
+++ webtable_to_text.rb
@@ -14,7 +14,7 @@ OptionParser.new do |opts|
   opts.on("-a", "--all", "Print all tables found on the specified page") { options[:all] = true }
   opts.on("-n", "--number NUM", "Print specific table number only; separate multiple numbers with commas") { |v| options[:number] = v }
   opts.on("-c", "--csv", "Output in CSV / comma separated values format") { options[:csv] = true }
-  opts.on("-f", "--file FILE", "Specify HTML file as source for extracting tables") { |v| options[:file] = v }
+  opts.on("-f", "--file FILE", "Specify HTML input file as source for extracting tables") { |v| options[:file] = v }
   opts.on("-i", "--interactive", "Interactive mode") { options[:interactive] = true }
   opts.on("-t", "--tsv", "Output in TSV / tab separated values format (default)") { options[:tsv] = true }
   opts.on("-m", "--markdown", "Output in markdown format") { options[:markdown] = true }
@@ -24,19 +24,28 @@ OptionParser.new do |opts|
 
 end.parse!
 
-url = options[:url]
+source = ""
 
-if !url
-  abort("  Please provide a url")
+url = options[:url]
+file = options[:file]
+
+if url
+  source_location = url
+  source_content = open(URI.encode(url)).read
+elsif file
+  source_location = file
+  source_content = File.read(file)
+else
+  abort("  Please provide a source file or URL as input")
 end
 
-doc = Nokogiri::HTML(open(URI.encode(url)))
+doc = Nokogiri::HTML(source_content)
 
 tables = doc.xpath('//table')
 len = tables.length
 
 if len < 1
-  abort("  No tables found at #{url}")
+  abort("  No tables found in page at #{source_location}")
 end
 
 numstring = "all"
@@ -45,7 +54,7 @@ if options[:number]
 end
 
 if options[:interactive]
-  puts "  There were #{len.to_s} tables found at the URL: #{url}"
+  puts "  There were #{len.to_s} tables found in the page at: #{source_location}"
   puts "  Please enter the number of the table you would like to convert, or <Enter> for all:"
   numstring = $stdin.gets.chomp
 end
